cf3e3aa6b864a22595af379c1d7e18a374034c1f
hengfeiyang <hengfei.yang@gmail.com>
2022-09-16 14:56:52 +0800

    update version

--- src/async_runner/runner_actix_web.rs
+++ src/async_runner/runner_actix_web.rs
@@ -1,10 +1,10 @@
 use futures::future::Future;
 use std::io::Error;
 
-pub async fn actix_main(actix_future: impl Future<Output = Result<(), Error>>) -> std::io::Result<()>
-{
- let fake_future = futures::future::ok::<(), ()>(());
- let r = futures::future::join(actix_future, fake_future);
- r.await
-  .0
+pub async fn actix_main(
+    actix_future: impl Future<Output = Result<(), Error>>,
+) -> std::io::Result<()> {
+    let fake_future = futures::future::ok::<(), ()>(());
+    let r = futures::future::join(actix_future, fake_future);
+    r.await.0
 }
--- src/error.rs
+++ src/error.rs
@@ -3,12 +3,10 @@ use thiserror::Error;
 use tonic::transport::Error as TError;
 
 #[derive(Error, Debug)]
-pub enum ActixWebTonicError
-{
- #[error("actix-web either tonic has an error; actix=`{actix:?}` tonic=`{tonic:?}`")]
- Either
- {
-  actix: Result<(), AError>,
-  tonic: Result<(), TError>
- }
+pub enum ActixWebTonicError {
+    #[error("actix-web either tonic has an error; actix=`{actix:?}` tonic=`{tonic:?}`")]
+    Either {
+        actix: Result<(), AError>,
+        tonic: Result<(), TError>,
+    },
 }
